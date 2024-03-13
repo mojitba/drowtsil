0f0d866ff6d16a4be405148a10ea5547497ecc83
mojitba <mojitba@gmail.com>
2024-03-13 17:48:24 +0330

    Add  docstrings to main file and helper file and minor fixes

--- drowtsil.py
+++ drowtsil.py
@@ -1,10 +1,41 @@
-""""Docstring about this script
+#!/usr/bin/env python3
+"""This script allows the user to create wordlist for targeted subject. Use only for educational or security audit purposes.
+
+This tool accepts two list in form of text file (.txt) or from command line and returns a wordlist text file.
+
+Program:
+    Drowtsil(reverse of word list)-v1.0 - Another wordlist for education or security audit purposes
+
+Usage:
+    python drowtsil.py -h
+
+Author:
+    Mojtaba Hemmati
+    github.com/mojitba 3/13/2024
+
+License:
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+See 'LICENSE' for more information.
 
 """
 from timeit import default_timer
 import argparse
 from pathlib import Path
 from itertools import permutations
+import re
 from errno import ENOSPC
 from multiprocessing import (
     cpu_count,
@@ -13,10 +44,8 @@ from multiprocessing import (
     set_start_method,
     Semaphore,
 )
-import re
 
 from modules import helpers
-
 
 logo = """
     ____                     __       _ __
@@ -31,6 +60,15 @@ logo = """
 
 
 def _add_pattern(word, pattern_dict):
+    """Add pattern which provided by the user to specified index of word
+
+    :param word: a word from wordlist
+    :type word: str
+    :param pattern_dict: a dictionary of pattern  in shape {index:"pattern"} (index 0 for first of word, 1000 for end of word)
+    :type pattern_dict: dict
+    :returns: a word with appended pattern
+    :rtype: str
+    """
     for i in pattern_dict.keys():
         if i == 0:
             word = pattern_dict.get(i) + word
@@ -42,6 +80,14 @@ def _add_pattern(word, pattern_dict):
 
 
 def _create_parser(process_number_default):
+    """Create and return a parser (argparse.ArgumentParser instance) for main() function.
+
+    :param process_number_default: default value for the number of process (compute with multiprocessing.cpu_count())
+    :type input_list: int
+    :returns: an instance from argparse.ArgumentParser class contains switchs with their values.
+    :rtype: <class 'argparse.ArgumentParser'>
+    """
+
     parser = argparse.ArgumentParser(
         description="Another Wordlist Generator for Security Audit Purposes",
         prog="Drowtsil WordList Generator",
@@ -109,7 +155,7 @@ def _create_parser(process_number_default):
         type=int,
         nargs="?",
         const=process_number_default,
-        default=0,
+        default=None,
         help="Number of processes in pool(default is number of logical cores - 1)",
     )
     parser.add_argument(
@@ -128,18 +174,18 @@ def _create_parser(process_number_default):
         "-c", "--capital", action="store_true", help="Enable capitalize function"
     )
     parser.add_argument(
-        "-sb",
-        "--substitution",
+        "-lt",
+        "--leet_case",
         action="store_true",
-        help="Enable substitution function, replce 'a' with '@', 's' with '$' and etc",
+        help="Enable leet case function, replce 'a' with '@', 's' with '$' and etc",
     )
     # ?
     parser.add_argument(
         "-t",
         "--toggle",
         type=int,
-        default=0,
-        const=1,
+        default=None,
+        const=0,
         nargs="?",
         help="Enable toggle case function with index [default=0]",
     )
@@ -163,6 +209,31 @@ def _create_parser(process_number_default):
 
 
 def create_wordlist(iter, min, max, level, capital, regexp, pattern_dict, word_counter):
+    """Get a iterable object and create a wordlist with selected options 
+    
+    Create wordlist by joining items from iterators in one string. every iterator contains a list of all permutations
+    tuples. if specific pattern is provided, it will be added and then pass to helper function (_add_to_wordlist). if
+    level selected by the user is 2 and capital switch doesn't disable, capitalized words also are added to wordlist.
+
+    :param iter: iterable returns from itertools.permutations
+    :type iter: itertools.permutations
+    :param min: minimum size of word by char
+    :type min: int
+    :param max: maximum size of word by char
+    :type max: int
+    :param level: user's selected level
+    :type level: int
+    :param capital: enable capitalize case transformation
+    :type capital: bool
+    :param regexp: regex pattern to match
+    :type regexp: str
+    :param pattern_dict: pattern provided by the user
+    :type pattern_dict: dict
+    :param word_counter: tracking generated words number
+    :type word_counter: int
+    :returns: returns a list of generated words and their number
+    :rtype: tuple
+    """
     wordlist = []
     for item in iter:
         word = "".join(item)
@@ -184,6 +255,23 @@ def create_wordlist(iter, min, max, level, capital, regexp, pattern_dict, word_c
 
 
 def _add_to_wordlist(wordlist, word, min, max, regexp, word_counter):
+    """A private function that adds a word to wordlist.
+
+    :param wordlist: an empty list for adding words 
+    :type wordlist: list
+    :param word: generated word that is going to add to wordlist
+    :type word: str
+    :param min: minimum size of word by char
+    :type min: int
+    :param max: maximum size of word by char
+    :type max: int
+    :param regexp: regex pattern to match
+    :type regexp: str
+    :param word_counter: tracking generated words number
+    :type word_counter: int
+    :returns: returns number of generated words
+    :rtype: int
+    """
     if len(word) >= min and len(word) <= max:
         if regexp != None:
             if re.match(regexp, word):
@@ -198,6 +286,26 @@ def _add_to_wordlist(wordlist, word, min, max, regexp, word_counter):
 
 
 def level_zero(args, current_words):
+    """It performs when user's selected level is 0. get a list of words and arguments provided by the user and apply selected functions on
+      all items.
+
+    list of functions: 
+      -upper case
+      -lower case,
+      -toggle case
+      -swap case
+      -capitalize case
+      -leet case
+      -reverse case
+      -alternating case 
+
+    :param args: arguments provide by the user
+    :type args: <class '__main__.Namespace'>
+    :param current_words: a merged list of constant words and temporary words
+    :type current_words: list
+    :returns: a tuple contains a list of transformed words and a list of operations that have been done 
+    :rtype: tuple
+    """
     words = []
     output_print = []
     if args.upper:
@@ -206,7 +314,7 @@ def level_zero(args, current_words):
     if args.lower:
         words += helpers.lower_case(current_words)
         output_print.append("lower case")
-    if args.toggle:
+    if args.toggle is not None:
         words += helpers.toggle_case(current_words, args.toggle)
         output_print.append("toggle case")
     if args.swap:
@@ -215,16 +323,15 @@ def level_zero(args, current_words):
     if args.capital:
         words += helpers.capitalize(current_words)
         output_print.append("capitalize")
-    if args.substitution:
-        words += helpers.substitution(current_words)
-        output_print.append("substitution")
+    if args.leet_case:
+        words += helpers.leet_case(current_words)
+        output_print.append("leet_case")
     if args.reverse:
         words += helpers.reverse(current_words)
         output_print.append("reverse")
     if args.alternating:
         words += helpers.alternating_case(current_words)
         output_print.append("alternating")
-    return set(words), output_print
     return set(words), output_print
 
 
@@ -236,10 +343,36 @@ def level_two(
     semaphore,
     write_function,
 ):
+    """It performs when user's selected level is 2. get a list of words and arguments provided by the user and apply selected functions on
+      every item of wordlist.
+
+      list of functions: 
+      -upper case
+      -sentence case
+      -toggle case
+      -capitalize case
+      -leet case
+      -alternating case 
+
+    :param wordlist: a list contains all permutations of input words
+    :type wordlist: list
+    :param args: arguments provided by the user
+    :type args: <class '__main__.Namespace'>
+    :param output_dir: path to output directory
+    :type output_dir: str
+    :param word_counter: tracking generated words number
+    :type word_counter: int
+    :param semaphore: semaphore for prevent of mutual exclusion (default is 1)
+    :type semaphore: multiprocessing.synchronize.Semaphore
+    :param write_function: an instance of helpers.write_to_file function
+    :type write_function: function
+    :returns: returns number of generated words
+    :rtype: int
+    """
     write_function(wordlist, semaphore, output_dir)
     temp_wordlist = []
-    if not args.substitution:
-        temp_wordlist += helpers.substitution(wordlist)
+    if not args.leet_case:
+        temp_wordlist += helpers.leet_case(wordlist)
         write_function(temp_wordlist, semaphore, output_dir)
         word_counter += len(temp_wordlist)
         temp_wordlist.clear()
@@ -248,13 +381,18 @@ def level_two(
         write_function(temp_wordlist, semaphore, output_dir)
         word_counter += len(temp_wordlist)
         temp_wordlist.clear()
-    if not args.sentence_case:
+    if not args.sentence:
         temp_wordlist += set(helpers.sentence_case(wordlist))
         write_function(temp_wordlist, semaphore, output_dir)
         word_counter += len(temp_wordlist)
         temp_wordlist.clear()
     if not args.toggle:
         temp_wordlist += set(helpers.toggle_case(wordlist, index=args.toggle))
+        write_function(temp_wordlist, semaphore, output_dir)
+        word_counter += len(temp_wordlist)
+        temp_wordlist.clear()
+    if not args.alternating:
+        temp_wordlist += set(helpers.alternating_case(wordlist))
         write_function(temp_wordlist, semaphore, output_dir)
         word_counter += len(temp_wordlist)
         temp_wordlist.clear()
@@ -266,6 +404,17 @@ def level_two(
 
 # private function for unittesting purposes
 def _extract_user_input(args, parser, read_function):
+    """A private function that reads input words from files and returns apropriate message if files not found.
+
+    :param args: arguments provided by the user
+    :type args: <class '__main__.Namespace'>
+    :param parser: an instance of argparse.ArgumentParser
+    :type parser: <class 'argparse.ArgumentParser'>
+    :param read_function: an instance of helpers.read_from_file function
+    :type read_function: function
+    :returns: a tuple containing a list of constant words and a list of temporary words
+    :rtype: tuple
+    """
     try:
         if args.filename and args.tmpfile:
             target_cons_dir = Path(args.filename)
@@ -296,11 +445,23 @@ def _extract_user_input(args, parser, read_function):
             1,
             message="[!] ERROR: Uncorrect specidfied path for input files. Try again!\n",
         )
+    except PermissionError:
+        parser.exit(
+            1,
+            message="[!] ERROR: Permission denied to specidfied path for input files. Try again!\n",
+        )
     return cons_words, tmp_words
 
 
 # private function for unittesting purposes
 def _extract_pattern(args):
+    """A private function that creates a dictionary of patterns which provided by the user.
+
+    :param args: arguments provided by the user
+    :type args: <class '__main__.Namespace'>
+    :returns: a dictionary of patterns {index: "pattern",}
+    :rtype: dict
+    """
     if args.pattern:
         pattern_dict = dict()
         for p in range(int(len(args.pattern) / 2)):
@@ -313,7 +474,18 @@ def _extract_pattern(args):
 
 
 # private function for unittesting purposes
-def _compute_total(tmp_words, len_input, pernumber):
+def _calculate_total(tmp_words, len_input, pernumber):
+    """A private function that calculates the total number of iteration to use in helpers.printProgressBar function.
+
+    :param tmp_words: a list of temporary words which each will be added to constant words to create current word list
+    :type tmp_words: list
+    :param len_input: length of constant word list
+    :type len_input: int
+    :param pernumber: minumum length permutations of elements in the iterable
+    :type pernumber: int
+    :returns: an integer that will be passed to helpers.printProgressBar function
+    :rtype: int
+    """
     if tmp_words:
         total = ((len_input + 2) - pernumber) * len(tmp_words)
     else:
@@ -321,7 +493,14 @@ def _compute_total(tmp_words, len_input, pernumber):
     return total
 
 
-def main(argv=None):
+def main(args=None):
+    """Main function to generated wordlist
+
+    :param args: arguments provided by the user - optional
+    :type args: <class '__main__.Namespace'>
+    :returns: None
+    :rtype: None
+    """
     start_method = get_all_start_methods()
     set_start_method(start_method[0])
     start_time = default_timer()
@@ -332,7 +511,7 @@ def main(argv=None):
     args = parser.parse_args()
     cons_words, tmp_words = _extract_user_input(args, parser, read_function)
     len_input = len(cons_words)
-    total = _compute_total(tmp_words, len_input, args.pernumber)
+    total = _calculate_total(tmp_words, len_input, args.pernumber)
     output_dir = Path(args.output)
     semaphore = Semaphore(1)
     iteration_number = 0
--- modules/helpers.py
+++ modules/helpers.py
@@ -1,6 +1,34 @@
-# helperss Module
+#!/usr/bin/env python3
+"""Helpers module contains functions that used to transform words and write generated wordlist to file
 
+Program:
+    Drowtsil(reverse of word list)-v1.0 - Another wordlist for education or security audit purposes
 
+Usage:
+    python drowtsil.py -h
+
+Author:
+    Mojtaba Hemmati
+    github.com/mojitba 3/13/2024
+
+License:
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+See 'LICENSE' for more information.
+
+"""
 substitutions = {
     "a": "@",
     "s": "$",
@@ -14,6 +42,13 @@ substitutions = {
 
 
 def alternating_case(input_list):
+    """Transform every item from input list to alternating case. In this form text alternates between lowercase and upper case.
+
+    :param input_list: wordlist provided by the user
+    :type input_list: list
+    :returns: a list of alternating version of input items
+    :rtype: list
+    """
     alternating_list = []
     for item in input_list:
         word = ""
@@ -29,10 +64,24 @@ def alternating_case(input_list):
 
 
 def capitalize(input_list):
+    """Capitalize case of every item from input list.
+
+    :param input_list: wordlist provided by the user
+    :type input_list: list
+    :returns: a list of capitalized version of input items
+    :rtype: list
+    """
     return [item.capitalize() for item in input_list]
 
 
 def _check_input(input_item):
+    """cheks if input is a string litteraly or an integer as a string.
+
+    :param input_item: input item
+    :type input_item: str
+    :returns: True if input is a string and False if not.
+    :rtype: bool
+    """
     try:
         if type(int(input_item)) == int:
             return False
@@ -41,28 +90,82 @@ def _check_input(input_item):
 
 
 def lower_case(input_list):
+    """Transforms every item from input list to lower case.
+
+    :param input_list: wordlist provided by the user
+    :type input_list: list
+    :returns: a list of lower case version of input items
+    :rtype: list
+    """
     return [item.lower() for item in input_list]
 
 
 def read_from_file(file_path):
+    """Reading wordlist provided by the user from file line by line.
+
+    :param file_path: location of input file
+    :type file_path: list
+    :returns: None
+    :rtype: None
+    """
     with open(file_path, "r") as f:
         inputs = f.read().splitlines()
     return inputs
 
 
 def reverse(input_list):
+    """Reverse every item from input.
+
+    :param input_word: list provided by the user
+    :type input_word: str
+    :returns: reverse version of input word
+    :rtype: str
+    """
     return [item[::-1] for item in input_list]
 
 
 def swap_case(input_list):
+    """Transforms every item from input list to swap case (upper to lower and vice versa).
+
+    :param input_list: wordlist provided by the user
+    :type input_list: list
+    :returns: a list of swap case version of input items
+    :rtype: list
+    """
     return [item.swapcase() for item in input_list]
 
 
 def sentence_case(input_list):
-    return capitalize(input_list)
+    """Transform every item from input list to sentence case.
+    
+    It works by capitalizing the very first letter in each sentence and then transform the rest of text in lowercase.
+    example: ["thisistext", "tHisIsALSOateXt"] Transforms to ['Thisistext', 'Thisisalsoatext']
+
+    :param input_list: wordlist provided by the user
+    :type input_list: list
+    :returns: a list of input items in sentence case version
+    :rtype: list
+    """
+    sentence_list = []
+    for item in input_list:
+        sentence_word = item[:1].capitalize()
+        sentence_word += item[1:].lower()
+        sentence_list.append(sentence_word)
+
+    return sentence_list
 
 
-def substitution(input_list):
+def leet_case(input_list):
+    """Transforms every item from input list to leet case(1337). 
+    
+    It uses substitutions dictionary to subtitute a to @, s to $ and etc.
+    example: ["riCk", "moRty", "42"] Transforms to ["r1Ck", "m0Rty", "moR7y", "m0R7y"]
+
+    :param input_list: wordlist provided by the user
+    :type input_list: list
+    :returns: a set of leet version of input items
+    :rtype: set
+    """
     new_list = []
     for word in input_list:
         if _check_input(word):
@@ -79,6 +182,18 @@ def substitution(input_list):
 
 
 def toggle_case(input_list, index):
+    """Transforms every item from input to toggle case with specified index. 
+    
+    in toggle case each word begins in lower case and the rest in upper case. 
+    example: toggle_case(input_list = ["rick"], index=0) returns ["rICK"]
+
+    :param input_list: wordlist provided by the user
+    :type input_list: list
+    :param index: index of string which Transforms to lower case
+    :type index: int
+    :returns: a list of toggle case version of input items
+    :rtype: list
+    """
     output_list = []
     for item in input_list:
         if _check_input(item):
@@ -95,10 +210,28 @@ def toggle_case(input_list, index):
 
 
 def upper_case(input_list):
+    """Transforms every item from input to upper case.
+
+    :param input_list: wordlist provided by the user
+    :type input_list: list
+    :returns: a list of upper case version of input items
+    :rtype: list
+    """
     return [item.upper() for item in input_list]
 
 
 def write_to_file(wordlist, semaphore, file_path):
+    """Write generated wordlist to file.
+
+    :param wordlist: wordlist to write on disk
+    :type wordlist: list
+    :param semaphore: semaphore for prevent of mutual exclusion (default is 1)
+    :type semaphore: multiprocessing.synchronize.Semaphore
+    :param file_path: location of output file
+    :type file_path: str
+    :returns: None
+    :rtype: None
+    """
     with semaphore:
         with open(file_path, "a") as file:
             for line in wordlist:
@@ -116,6 +249,27 @@ def printProgressBar(
     fill="█",
     printEnd="\r",
 ):
+    """Call in a loop to create terminal progress bar.
+
+    :param iteration: current iteration - Required
+    :type iteration: int
+    :param total: total iterations - Required
+    :type total: int
+    :param prefix: prefix string - Optional
+    :type prefix: str
+    :param suffix: suffix string - Optional
+    :type suffix: str
+    :param decimals: positive number of decimals in percent complete - Optional
+    :type decimals: int
+    :param length: character length of bar - Optional
+    :type length: int
+    :param fill: bar fill character - Optional
+    :type fill: str
+    :param printEnd: end character (e.g. "\r", "\r\n") - Optional
+    :type printEnd: str
+    :returns: None
+    :rtype: str
+    """
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + "-" * (length - filledLength)
